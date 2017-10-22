281e2a382fc7403aa7bac21726b4399ee54e6e8c
Shishir <shishirshetty0710@gmail.com>
2017-10-21 23:12:02 -0400

    Snackbar alert to user if a number is not recognized Menu bar option to try a new number Menu bar option to manually type a number

--- app/src/main/java/com/shishir/sieveoferatosthenes/MainActivity.java
+++ app/src/main/java/com/shishir/sieveoferatosthenes/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -166,5 +168,29 @@ public class MainActivity extends AppCompatActivity {
         return number;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);//Menu Resource, Menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+
+                recyclerView.setVisibility(View.GONE);
+                ll_mic.setVisibility(View.VISIBLE);
+
+                return true;
+            case R.id.item2:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
